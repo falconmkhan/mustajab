0cff381f3a09f9f4bf0ddfb6594f9b115a86be8a
falkonmkhan <r2whiphop@gmail.com>
2024-03-09 19:11:48 -0800

    project upload

---
+++ js/home.js
@@ -0,0 +1,24 @@
+// let slides = document.querySelectorAll(".slide")
+
+// slides.forEach((slide,idx)=>{
+//       console.log(slide , idx);
+//          slide.style.left = `${idx * 100}%`;
+//         slide.style.bottom = `${idx * 100}%`;
+//     });
+    
+//     let prev = ()=>{
+//     counter--;
+//     image();
+//     };
+    
+//     let next = ()=>{
+//     counter++;
+//     image();
+//     };
+    
+    
+//     let image = ()=>{
+//         slides.forEach((slide)=>{
+//         slide.style.transform = `translateX(-${counter * 100}%)`;
+//        })
+//     }
